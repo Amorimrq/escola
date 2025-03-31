95ede13dda94d35642ed94552156bf6029a82f78
amorim <amorimrq@gmail.com>
2025-03-31 18:32:16 -0300

    exercicio for

--- cu.py
+++ cu.py
@@ -1,1 +1,9 @@
-print('cu')
+print("______Tabuada_____")
+
+
+for numero in range(1,11):
+    print(f"Tabuada do {numero}")
+    for nu in range(0,11):    
+        print(nu*numero)
+    print("_____________")
+    print("")
