a22de2c53f287f185be0d80ede159bc1de99dc44
amorim <amorimrq@gmail.com>
2025-04-02 16:41:55 -0300

    exercicios

---
+++ letra.py
@@ -0,0 +1,14 @@
+palavra = 'Pedro'
+
+for letra in palavra:
+    print(letra)
+
+print("_______________________________________________")
+
+numeros = [1,2,3,4,5]
+soma = 0
+
+for numero in numeros:
+    soma = numeros + numeros
+
+print(soma)
